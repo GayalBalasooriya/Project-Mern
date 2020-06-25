2affaea1613928e41f2aee6fc54e3bc1233456f8
Gayal Balasooriya <baigayal01@gmail.com>
2020-06-25 23:57:08 +0530

    added delete education, experiences and account

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -145,3 +145,26 @@ export const addEducation = (formData, history) => async dispatch => {
         })
     }
 }
+
+//Delete experience
+export const deleteExperience = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/experience/${id}`)
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        })
+
+        dispatch(setAlert('Education Added', 'success'))
+
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { 
+                msg: err.response.statusText, 
+                status: err.response.status 
+            }
+        })
+    }
+}
